fb7f43efd3de9a6cd9fbf5775c48940cb0e76cf2
Paul Loyd <pavelko95@gmail.com>
2018-01-16 00:40:45 +0300

    Replace "glsl" rollup plugin plugin with "glslify"

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import glsl from 'rollup-plugin-glsl';
+import glslify from 'rollup-plugin-glslify';
 import uglify from 'rollup-plugin-uglify';
 import istanbul from 'rollup-plugin-istanbul';
 import pkg from './package.json';
@@ -14,12 +14,13 @@ const is = x => target === x;
 
 const entry = is('test') || is('cov') ? 'tests/index' : 'src/index';
 
-const glslFiles = ['src/shaders/**.vert', 'src/shaders/**.frag'];
+const glslFiles = ['src/**/*.vert', 'src/**/*.frag'];
 const jsFiles = ['src/**/*.js'];
 
 const plugins = [
     babel({exclude: glslFiles}),
-    glsl({include: glslFiles}),
+    // TODO: use normal relative paths.
+    glslify({include: glslFiles, basedir: './src/'}),
     resolve(),
 ];
 
@@ -33,9 +34,10 @@ const umd = {
     },
     name: pkg.name,
     plugins: [
+        // TODO: reorder plugins.
         is('cov') && istanbul({include: jsFiles}),
-        ...plugins,
         commonjs(),
+        ...plugins,
         is('prod') && uglify(),
     ],
     sourcemap,
